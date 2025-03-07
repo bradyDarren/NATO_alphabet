94d56558028670990061846b828178d7c8af996d
bradyDarren <dcgbfinancial@gmail.com>
2025-03-06 22:57:45 -0600

    addition of try and expect blocks

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ nato_dict = {row.letter:row.code for (index,row) in nato.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs. 
 
-user_input = list(input("Input a series of letter to have translated into phonetic code: ").upper()) 
+def generate_phonetic():
+    user_input = list(input("Input a series of letter to have translated into phonetic code: ").upper()) 
+    try:
+        code_list = [nato_dict[letter] for letter in user_input ]
+    except KeyError: 
+        print("Sorry. Only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(code_list)
 
-code_list = [nato_dict[letter] for letter in user_input ]
-
-print(code_list)
+generate_phonetic()
